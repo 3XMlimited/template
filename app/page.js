02ae7e795a382d4c3519d119048636a55ca11d5e
tt <sydney118227@icloud.com>
2023-12-27 16:07:46 +0800

    3.0

--- app/page.js
+++ app/page.js
@@ -41,10 +41,10 @@ export default function Home() {
       },
     });
     const result = await response.json();
-    for (let i = 0; i < result.length; i++) {
-      result[i].subscriptions = await fetchConvertKit(result[i].forms);
-    }
-    console.log(result);
+    // for (let i = 0; i < result.length; i++) {
+    //   result[i].subscriptions = await fetchConvertKit(result[i].forms);
+    // }
+    console.log("result", result);
 
     setData(
       result.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt))
@@ -122,10 +122,10 @@ export default function Home() {
           </div>
           <div className="grid  grid-cols-4 gap-20">
             {data?.map((d, i) => (
-              <div className="border-2 h-[300px] pb-4 rounded-2xl">
+              <div className="border-2 h-[300px] pb-4 rounded-2xl flex justify-center">
                 <ContextMenu key={i}>
                   <ContextMenuTrigger>
-                    <div className="w-[220px] h-[220px] hover:bg-slate-100 relative bg-slate-50 rounded-lg ">
+                    <div className="w-[220px] h-[220px] hover:bg-slate-100 relative bg-slate-50 rounded-lg mt-5 ">
                       <Link href={`/${d._id}`}>
                         <div className="w-full h-full  flex justify-center items-center">
                           <Image
@@ -141,11 +141,11 @@ export default function Home() {
                       <h1 className="flex justify-center mt-1 font-bold ">
                         {d.topic}
                       </h1>
-                      <div className="flex justify-center mb-2 ">
+                      {/* <div className="flex justify-center mb-2 ">
                         <div className="bg-[#112233] text-white py-2 px-4 rounded-md">
                           {d?.subscriptions}
                         </div>
-                      </div>
+                      </div> */}
                     </div>
                   </ContextMenuTrigger>
                   <ContextMenuContent>
